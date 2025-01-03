d99067638b6c45c239f64fbec0ea446447adb5e8
Yago Dias <yago.carioca@hotmail.com>
2025-01-03 18:44:48 -0300

    Refatoração da Home
    - Alterado nome da Activity;
    - Setada cor branca para o titulo da Toolbar;

--- app/src/main/java/com/fukajima/warframemarket/HomeActivity.kt
+++ app/src/main/java/com/fukajima/warframemarket/HomeActivity.kt
@@ -3,14 +3,10 @@ package com.fukajima.warframemarket
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.fukajima.warframemarket.fragments.Fragment_Market
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     lateinit var navigation: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,11 +24,11 @@ class MainActivity : AppCompatActivity() {
                     return@setOnItemSelectedListener true
                 }
                 R.id.nav_menu_contracts -> {
-                    Toast.makeText(this@MainActivity, "Not Implemented", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@HomeActivity, "Not Implemented", Toast.LENGTH_SHORT).show()
                     return@setOnItemSelectedListener true
                 }
                 R.id.nav_menu_profile -> {
-                    Toast.makeText(this@MainActivity, "Not Implemented", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@HomeActivity, "Not Implemented", Toast.LENGTH_SHORT).show()
                     return@setOnItemSelectedListener true
                 }
                 else -> {
